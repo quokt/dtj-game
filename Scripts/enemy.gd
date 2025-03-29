8bf8e77df29658964dc64246b47393f1054d5957
Quokt <bug87@hotmail.fr>
2025-03-29 21:04:07 +0100

    blob

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 
-@onready var target : Tower = get_tree().get_nodes_in_group("towers")[randi()%get_tree().get_nodes_in_group("towers").size()-1]
+@onready var target : Vector2 = get_tree().get_first_node_in_group("dancefloor").position + Vector2(randf_range(-200.0, 200.0), randf_range(-50.0,50.0))
 var speed = 800.0
 
 
 func _physics_process(delta: float) -> void:
-	velocity = (target.position - position).normalized() * speed * delta
+	velocity = (target - position).normalized() * speed * delta
 	move_and_slide()

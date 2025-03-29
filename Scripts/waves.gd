e0f8b831f4d32c4fbb1c9ab9649457db64147e2c
Quokt <bug87@hotmail.fr>
2025-03-29 16:32:56 +0100

    beatwaves

--- Scripts/waves.gd
+++ Scripts/waves.gd
@@ -1,37 +1,38 @@
 extends Node2D
 
-const WAVE_LIFETIME = 5.0
+@export var WAVE_LIFETIME = 5.0
 
 var offset := 100.0
 
-@export var wave_speed : float = 300.0
+@export var wave_speed : float
+@export var wave_accel : float
+@export var thickness : float 
 
 var waves_array := []
 
 class Wave:
-	var lifetime : float = 0.0
+	var elapsed : float = 0.0
+	var lifetime : float
 
 func _draw() -> void:
 	for tower in %Towers.get_children():
-		var color : Color = Color.GREEN
+		var color : Color = Color.TURQUOISE
 		if tower == %Table : color = Color.RED
 		for wave in waves_array:
-			draw_circle(tower.global_position, wave.lifetime*600.0, color, false, sin(get_process_delta_time())*100.0)
-
+			draw_circle(tower.global_position, (wave.elapsed*wave.elapsed*wave_accel)*wave_speed*get_physics_process_delta_time(), color, false, (wave.lifetime-wave.elapsed)*(wave.lifetime-wave.elapsed)*thickness*get_physics_process_delta_time())
 
 func _physics_process(delta: float) -> void:
 	queue_redraw()
 	for wave in waves_array:
 		#print(wave.lifetime)
-		wave.lifetime += get_physics_process_delta_time()
-		if wave.lifetime >= WAVE_LIFETIME:
+		wave.elapsed += delta
+		if wave.elapsed >= wave.lifetime:
 			waves_array.erase(wave)
 
 
 func _on_beat() -> void:
 	#print(self)
 	var new_wave := Wave.new()
+	new_wave.lifetime = WAVE_LIFETIME
 	waves_array.append(new_wave)
-	
-	
 	return

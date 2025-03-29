d12541e7cf75e319efaab6cf900fbe2dbfe8a3a7
Quokt <bug87@hotmail.fr>
2025-03-29 21:35:35 +0100

    blabla

---
+++ Scripts/mouse.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+
+func _physics_process(delta: float) -> void:
+	position = get_global_mouse_position()
--- Scripts/speaker.gd
+++ Scripts/speaker.gd
@@ -1,6 +1,26 @@
 extends Tower
 
+signal clicked(ref: Tower)
+
+var mouse_inside : bool = false
+
+var active : bool = false
+
 var elapsed : float = 0.0
 func _process(delta: float) -> void:
 	elapsed += delta
 	$Sprite2D/ColorRect.material.set_shader_parameter("chaos", elapsed)
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.is_action_pressed("left_click") and mouse_inside:
+			print("clicked")
+			clicked.emit(self)
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	mouse_inside = true
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	mouse_inside = false

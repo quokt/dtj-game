72bbc22948b4dc0761a4432d24e1b2fb9a78b175
Quokt <bug87@hotmail.fr>
2025-03-30 13:18:06 +0200

    table collision

--- Scripts/table.gd
+++ Scripts/table.gd
@@ -1,1 +1,18 @@
 extends Tower
+
+signal clicked(ref : Tower)
+
+var mouse_inside : bool = false
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.is_action_pressed("left_click") and mouse_inside:
+			print("clicked")
+			clicked.emit(self)
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	mouse_inside = true
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	mouse_inside = false
